8c7a6074184bde01aa9991b72c5b7e847110078d
SAMUEL HAILE <samishken@gmail.com>
2018-06-13 13:37:30 -0400

    responses & reflections component rendering

--- app/components/index-component.js
+++ app/components/index-component.js
@@ -6,28 +6,28 @@ Encompass.IndexComponent = Ember.Component.extend({
   tagName: 'index',
   classNameBindings: ['homepage', 'index'],
   
-  init () {
-    this._super(...arguments)
-    console.log('init')
-  },
+  // init () {
+  //   this._super(...arguments)
+  //   console.log('init')
+  // },
 
-  didReceiveAttrs () {
-    this._super(...arguments)
-    console.log('did receive attrs')
-  },
+  // didReceiveAttrs () {
+  //   this._super(...arguments)
+  //   console.log('did receive attrs')
+  // },
 
-  willRender () {
-    this._super(...arguments)
-    console.log('will render')
-  },
+  // willRender () {
+  //   this._super(...arguments)
+  //   console.log('will render')
+  // },
 
-  didInsertElement () {
-    this._super(...arguments)
-    console.log('did insert element')
-  },
+  // didInsertElement () {
+  //   this._super(...arguments)
+  //   console.log('did insert element')
+  // },
 
-  didRender () {
-    this._super(...arguments)
-    console.log('did render')
-  }
+  // didRender () {
+  //   this._super(...arguments)
+  //   console.log('did render')
+  // }
 })
--- tests/integration/components/responses-component.js
+++ tests/integration/components/responses-component.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('response-reflection-component', 'Integration | Component | response reflection component', {
+moduleForComponent('responses-component', 'Integration | Component | index component', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{response-reflection-component}}`);
+  this.render(hbs`{{responses-component}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#response-reflection-component}}
+    {{#responses-component}}
       template block text
-    {{/response-reflection-component}}
+    {{/responses-component}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
