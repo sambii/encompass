4c42cc15fde1a6aca956171670e1c3087e3fd9e8
SAMUEL HAILE <samishken@gmail.com>
2018-06-13 16:20:42 -0400

    user component created

--- tests/integration/components/users-component.js
+++ tests/integration/components/users-component.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('responses-component', 'Integration | Component | responses component', {
+moduleForComponent('users-component', 'Integration | Component | users component', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{responses-component}}`);
+  this.render(hbs`{{users-component}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#responses-component}}
+    {{#users-component}}
       template block text
-    {{/responses-component}}
+    {{/users-component}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
