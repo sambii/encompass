c6ea86abbb02ac2ea2b18eaa1662826ccc7e9599
SAMUEL HAILE <samishken@gmail.com>
2018-07-11 13:11:24 -0400

    section create completed

--- app/routes/sections_route.js
+++ app/routes/sections_route.js
@@ -1,5 +1,10 @@
+/**
+  * # Sections Route
+  * @description Route for dealing with all section objects
+  * @todo This is really the sections_index route and should be named as such by convention
+  */
 Encompass.SectionsRoute = Encompass.AuthenticatedRoute.extend({
-  model: function (params) {
+  model: function () {
     var store = this.get('store');
     var sections = store.findAll('section');
     // Filter only problems by current logged in user
