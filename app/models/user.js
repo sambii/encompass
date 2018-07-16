e232b67b1f48b32edd978abdb3d1914e87b1b729
SAMUEL HAILE <samishken@gmail.com>
2018-07-16 17:40:26 -0400

    student array fixed

--- app/models/user.js
+++ app/models/user.js
@@ -10,6 +10,7 @@ Encompass.User = DS.Model.extend(Encompass.Auditable, {
   isGuest: DS.attr('boolean'),
   isAdmin: DS.attr('boolean'),
   isAuthorized: DS.attr('boolean'),
+  isStudent: DS.attr('boolean'),
   seenTour: DS.attr('date'),
   lastImported: DS.attr('date'),
   lastLogin: DS.attr('date'),
